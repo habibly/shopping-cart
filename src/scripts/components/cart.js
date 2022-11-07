7256e83a9c6b31f14963342ccb46e11a31207e30
habibly <ahmadhbb1995@gmail.com>
2022-11-07 23:05:38 +0500

    add backdrop behind cart

--- src/scripts/components/cart.js
+++ src/scripts/components/cart.js
@@ -10,24 +10,24 @@ export default class Cart extends Component {
       element: document.querySelector('#cart')
     })
     let self = this;
-    
+
     // toggle cart display
-    window.addEventListener('click', (e)=> {
-      if(e.target.dataset.type === 'cart-toggle'){
+    window.addEventListener('click', (e) => {
+      if (e.target.dataset.type === 'cart-toggle') {
         e.preventDefault()
         self.toggleCartDisplay()
       }
     })
 
-    window.addEventListener('submit', (e)=>{
-      if(e.target.dataset.type === 'add-to-cart-form'){
+    window.addEventListener('submit', (e) => {
+      if (e.target.dataset.type === 'add-to-cart-form') {
         e.preventDefault()
 
         let formData = new FormData(e.target)
         self.addToCart(formData)
       }
 
-      if(e.target.dataset.type === 'remove-product-cart-form'){
+      if (e.target.dataset.type === 'remove-product-cart-form') {
         e.preventDefault()
 
         let formData = new FormData(e.target)
@@ -39,28 +39,28 @@ export default class Cart extends Component {
     store.state.cart = JSON.parse(localStorage.getItem('cartItems')) || []
 
   }
-  toggleCartDisplay(){
+  toggleCartDisplay() {
     let self = this;
-    if(self.element.style.display === 'none'){
+    if (self.element.style.display === 'none') {
       self.element.style.display = 'block';
       document.body.style.overflowY = 'hidden';
 
-    }else {
+    } else {
       self.element.style.display = 'none'
       document.body.style.overflowY = 'auto'
     }
   }
-  addToCart(formData){
+  addToCart(formData) {
     let self = this;
 
     var formObject = {};
     formData.forEach((value, key) => formObject[key] = value);
-    
+
     store.dispatch('addToCart', formObject)
 
     self.toggleCartDisplay()
   }
-  removeFromCart(formData){
+  removeFromCart(formData) {
     store.dispatch('removeFromCart', formData.get('id'))
     console.log(store.state.cart)
   }
@@ -68,18 +68,19 @@ export default class Cart extends Component {
     let self = this;
 
     self.element.innerHTML = `
-      <div class="wrapper">
-        <div class="cart-header"><h3>Cart</h3><a href="#" data-type="cart-toggle">X</a></div>
-        <div class="cart-body"></div>
-      </div>
+    <div class="wrapper">
+      <div class="cart-header"><h3>Cart</h3><a href="#" data-type="cart-toggle">X</a></div>
+      <div class="cart-body"></div>
+    </div>
+    <div class="backdrop" data-type="cart-toggle"></div>
       `
 
     let cartBody = self.element.querySelector('.cart-body')
 
     if(store.state.cart.length === 0) {
       cartBody.innerHTML = `
-        <p> You cart is empty. Add some products from the list</p>
-      `
+      <p> You cart is empty. Add some products from the list</p>
+        `
       return;
     }
 
@@ -87,7 +88,7 @@ export default class Cart extends Component {
       
       let { id, title, price, thumbnail, quantity } = x;
       return `
-      <article id="cart-product-${id}">
+        <article id="cart-product-${id}">
       <a href="#">
         <figure>
           <img src="${thumbnail}" title="${title}" width="95" height="95">
@@ -105,7 +106,7 @@ export default class Cart extends Component {
         <input type="hidden" name="id" value="${id}">
         <button type="submit">Remove</button>
       </form>
-    </article>
+    </article >
       `
     }).join('')
   }
