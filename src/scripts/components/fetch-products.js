aa205f15c33e1bdd2f601e3d78375d9460066279
habibly <ahmadhbb1995@gmail.com>
2022-11-09 23:16:25 +0500

    update port in fetch request

--- src/scripts/components/fetch-products.js
+++ src/scripts/components/fetch-products.js
@@ -1,7 +1,7 @@
 export default class ProductData{
   constructor(){}
   async fetchData(page){
-    return fetch(`http://localhost:8000/products?_page=${page}&_limit=6`)
+    return fetch(`http://localhost:5000/products?_page=${page}&_limit=6`)
     .then( response => response.json() )
     .then(response => {
       return response
